54f8167b288e67d218301cde095f852df51733b3
Nicolle07M <marqueznicolle421@gmail.com>
2024-02-20 16:29:24 -0500

    Separando las funciones asincronas a los ficheros correspondientes en el directorio LOGIC. Exportando las funciones. Refactorizando los controladores.

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const Curso = require ('../models/curso_model');
-const ruta  = express.Router();
+const logic = require('../logic/curso_logic');
+const ruta = express.Router();
 
-//Funcion asincrona para listar los cursos activos
-async function listarCursosActivos(){
-    let cursos = await Curso.find({"estado": true});
-    return cursos;
-}
-
-//Endpoint de tipo GET para el recurso usuarios. Lista de todos los cursos
-ruta.get('/', (req, res) => {
-    let resultado = listarCursosActivos();
+// Endpoint de tipo GET para el recurso Cursos
+ruta.get('/',(req, res) => {
+    let resultado = logic.listarCursosActivos();
     resultado.then(cursos => {
         res.json(cursos);
     }).catch(err => {
@@ -18,22 +12,9 @@ ruta.get('/', (req, res) => {
     })
 });
 
-//Funcion asincrona para crear cursos
-
-async function crearCurso(body){
-    let curso = new Curso({
-        titulo: body.titulo,
-        descripcion: body.descripcion,
-        alumnos: body.alumnos,
-        calificacion: body.calificacion
-    });
-    return await curso.save();
-}
-
-
-//Endpoint de tipo POST para el recurso CURSOS
+// Endpoint de tipo POST para el recurso CURSOS
 ruta.post('/', (req, res) => {
-    let resultado = crearCurso(req.body);
+    let resultado = logic.crearCurso(req.body);
 
     resultado.then(curso => {
         res.json({
@@ -46,20 +27,9 @@ ruta.post('/', (req, res) => {
     })
 });
 
-// Funcion asincrona para actualizar cursos
-async function actualizarCurso(id, body){
-    let curso = await Curso.findByIdAndUpdate(id, {
-        $set: {
-            titulo: body.titulo,
-            descripcion: body.descripcion
-        }
-    }, {new: true});
-    return curso;
-}
-
-//Endpoint de tipo PUT para el recurso CURSOS
-ruta.put('/:id', (req, res) =>{
-    let resultado = actualizarCurso(req.params.id, req.body);
+// Endpoint de tipo PUT para el recurso CURSOS
+ruta.put('/:id', (req, res) => {
+    let resultado = logic.actualizarCurso(req.params.id, req.body);
     resultado.then(curso => {
         res.json(curso)
     }).catch(err => {
@@ -67,19 +37,9 @@ ruta.put('/:id', (req, res) =>{
     })
 })
 
-//Funcion asincrona para inactivar cursos
-async function desactivarCurso(id){
-    let curso = await Curso.findByIdAndUpdate(id, {
-        $set: {
-            estado: false
-        }
-    }, {new: true});
-    return curso;
-}
-
-//Endpoint de tipo DELETE para el recurso CURSOS 
+// Endpoint de tipo DELETE para el recurso CURSOS
 ruta.delete('/:id', (req, res) => {
-    let resultado = desactivarCurso(req.params.id);
+    let resultado = logic.desactivarCurso(req.params.id);
     resultado.then(curso => {
         res.json(curso);
     }).catch(err => {
@@ -87,8 +47,4 @@ ruta.delete('/:id', (req, res) => {
     })
 })
 
-
-
-
 module.exports = ruta;
-
