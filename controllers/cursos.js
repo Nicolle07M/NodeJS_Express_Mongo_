6c444ba9547a014b20f3547497f5a846c58aeb80
Nicolle07M <marqueznicolle421@gmail.com>
2024-02-20 13:33:12 -0500

    Agregando y probando la fucionalidad para editar cursos. Endpoint de tipo PUT

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -12,7 +12,7 @@ async function crearCurso(body){
     let curso = new Curso({
         titulo: body.titulo,
         descripcion: body.descripcion,
-        akumnos: body.alumnos,
+        alumnos: body.alumnos,
         calificacion: body.calificacion
     });
     return await curso.save();
@@ -33,6 +33,28 @@ ruta.post('/', (req, res) => {
         })
     })
 });
+
+// Funcion asincrona para actualizar cursos
+async function actualizarCurso(id, body){
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set: {
+            titulo: body.titulo,
+            descripcion: body.descripcion
+        }
+    }, {new: true});
+    return curso;
+}
+
+//Endpoint de tipo PUT para el recurso CURSOS
+ruta.put('/:id', (req, res) =>{
+    let resultado = actualizarCurso(req.params.id, req.body);
+    resultado.then(curso => {
+        res.json(curso)
+    }).catch(err => {
+        res.status(400).json(err)
+    })
+})
+
 
 
 module.exports = ruta;
--- models/curso_model.js
+++ models/curso_model.js
@@ -17,7 +17,7 @@ const cursoSchema = new mongoose.Schema({
         type: String,
         required: false
     },
-    alumos: {
+    alumnos: {
         type: Number,
         default: 0
     },
